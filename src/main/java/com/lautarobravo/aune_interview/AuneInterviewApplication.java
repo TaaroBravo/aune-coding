adb7484c1ad455173e41127828a2e483062192e9
Lautaro Bravo <lautabdls@gmail.com>
2024-11-21 16:38:19 -0300

    base project

---
+++ src/main/java/com/lautarobravo/aune_interview/AuneInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.lautarobravo.aune_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuneInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuneInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lautarobravo/aune_interview/AuneInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lautarobravo.aune_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuneInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
